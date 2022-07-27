eb0b81f6ce139175a63934483c4e3f638018bff6
unknown <tuantran21101998@gmail.com>
2022-07-28 00:25:15 +0700

    make it better

--- react-pdf-viewer/src/App.js
+++ react-pdf-viewer/src/App.js
@@ -1,52 +1,52 @@
-import React, {useState} from 'react'
-import { Document,Page } from 'react-pdf/dist/esm/entry.webpack';
+import React, { useState } from 'react'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import './App.css';
 function App() {
 
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({numPages}){
+  function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
     setPageNumber(1);
   }
 
-  function changePage(offSet){
+  function changePage(offSet) {
     setPageNumber(prevPageNumber => prevPageNumber + offSet);
   }
 
-  function changePageBack(){
+  function changePageBack() {
     changePage(-1)
   }
 
-  function changePageNext(){
+  function changePageNext() {
     changePage(+1)
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Document file="/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+      {/****<header className="App-header">
+        <Document file="/portfo.pdf" onLoadSuccess={onDocumentLoadSuccess}>
           <Page height="600" pageNumber={pageNumber} />
         </Document>
         <p> Page {pageNumber} of {numPages}</p>
-        { pageNumber > 1 && 
-        <button onClick={changePageBack}>Previous Page</button>
+        {pageNumber > 1 &&
+          <button onClick={changePageBack}>Previous Page</button>
         }
         {
           pageNumber < numPages &&
           <button onClick={changePageNext}>Next Page</button>
         }
-      </header>
+      </header>*/}
       <center>
         <div>
-          <Document file="/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+          <Document file="/portfo.pdf" onLoadSuccess={onDocumentLoadSuccess}>
             {Array.from(
               new Array(numPages),
-              (el,index) => (
-                <Page 
-                  key={`page_${index+1}`}
-                  pageNumber={index+1}
+              (el, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
                 />
               )
             )}
